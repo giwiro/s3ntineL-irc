6e0f5ec811ff8334fe72de20756ee82b3411e4a0
giwiro <giwirodavalos@gmail.com>
2020-09-06 04:00:36 -0500

    fixed styling and added cpu for windows

--- s3ntineL-irc/logger.cpp
+++ s3ntineL-irc/logger.cpp
@@ -10,59 +10,59 @@ void log_message(const CHAR* fmt, ...)
 void log_message(const char* fmt, ...)
 #endif
 {
-	// fflush(stdout);
+    // fflush(stdout);
 #ifdef _WIN32
-	static CHAR buffer[MAX_SIZE_LOGGER_BUFFER];
+    static CHAR buffer[MAX_SIZE_LOGGER_BUFFER];
 #elif defined(__linux__) || defined(__APPLE__)
-	char buffer[MAX_SIZE_LOGGER_BUFFER];
+    char buffer[MAX_SIZE_LOGGER_BUFFER];
 #endif
-	va_list args;
-	va_start(args, fmt);
+    va_list args;
+    va_start(args, fmt);
 #ifdef _WIN32
-	// Function to write the formatted string into another buffer
-	vsprintf_s(buffer, MAX_SIZE_LOGGER_BUFFER, fmt, args);
-	// static TCHAR bufferPrint[MAX_SIZE_LOGGER_BUFFER];
-	// Windows based output log (Into the Output tab in Visual Studio)
-	// OutputDebugString(buffer);
-	printf("%s", buffer);
+    // Function to write the formatted string into another buffer
+    vsprintf_s(buffer, MAX_SIZE_LOGGER_BUFFER, fmt, args);
+    // static TCHAR bufferPrint[MAX_SIZE_LOGGER_BUFFER];
+    // Windows based output log (Into the Output tab in Visual Studio)
+    // OutputDebugString(buffer);
+    printf("%s", buffer);
 #elif defined(__linux__) || defined(__APPLE__)
-	int n = vsnprintf(buffer, MAX_SIZE_LOGGER_BUFFER, fmt, args);
-	if (n < 0 || n > MAX_SIZE_LOGGER_BUFFER) {
-		printf("There was a problem logging...");
-		return;
-	}
-	printf("%s", buffer);
+    int n = vsnprintf(buffer, MAX_SIZE_LOGGER_BUFFER, fmt, args);
+    if (n < 0 || n > MAX_SIZE_LOGGER_BUFFER) {
+        printf("There was a problem logging...");
+        return;
+    }
+    printf("%s", buffer);
 #endif
-	// Facilitate a normal return when va_start is used
-	va_end(args);
-	fflush(stdout);
+    // Facilitate a normal return when va_start is used
+    va_end(args);
+    fflush(stdout);
 
 }
 
 void print_banner()
 {
-	printf("%s\n", "                      _                        ");
-	printf("%s\n", "                      \\`*-.                    ");
-	printf("%s\n", "                       )  _`-.                 ");
-	printf("%s\n", "                      .  : `. .                ");
-	printf("%s\n", "                      : _   '  \\               ");
-	printf("%s\n", "                      ; *` _.   `*-._          ");
-	printf("%s\n", "                      `-.-'          `-.       ");
-	printf("%s\n", "        0wn3d           ;       `       `.     ");
-	printf("%s\n", "                        :.       .        \\    ");
-	printf("%s\n", "                        . \\  .   :   .-'   .   ");
-	printf("%s\n", "                        '  `+.;  ;  '      :   ");
-	printf("%s\n", "                        :  '  |    ;       ;-. ");
-	printf("%s\n", "                        ; '   : :`-:     _.`* ;");
-	printf("%s\n", "                     .*' /  .*' ; .*`- +'  `*' ");
-	printf("%s\n", "                     `*-*   `*-*  `*-*'        ");
-/*
-	printf("%s\n", "    _                ___       _.--.");
-	printf("%s\n", "    \\`.|\\..----...-'`   `-._.-'_.-'`");
-	printf("%s\n", "    /  ' `         ,       __.--'");
-	printf("%s\n", "    )/' _/     \\   `-_,   /        ");
-	printf("%s\n", "    `-'\" `\"\\_  ,_.-;_.-\\_ ',     ");
-	printf("%s\n", "        _.-'_./   {_.'   ; /");
-	printf("%s\n", "       {_.-``-'         {_/");
-*/
+    printf("%s\n", "                      _                        ");
+    printf("%s\n", "                      \\`*-.                    ");
+    printf("%s\n", "                       )  _`-.                 ");
+    printf("%s\n", "                      .  : `. .                ");
+    printf("%s\n", "                      : _   '  \\               ");
+    printf("%s\n", "                      ; *` _.   `*-._          ");
+    printf("%s\n", "                      `-.-'          `-.       ");
+    printf("%s\n", "        0wn3d           ;       `       `.     ");
+    printf("%s\n", "                        :.       .        \\    ");
+    printf("%s\n", "                        . \\  .   :   .-'   .   ");
+    printf("%s\n", "                        '  `+.;  ;  '      :   ");
+    printf("%s\n", "                        :  '  |    ;       ;-. ");
+    printf("%s\n", "                        ; '   : :`-:     _.`* ;");
+    printf("%s\n", "                     .*' /  .*' ; .*`- +'  `*' ");
+    printf("%s\n", "                     `*-*   `*-*  `*-*'        ");
+    /*
+        printf("%s\n", "    _                ___       _.--.");
+        printf("%s\n", "    \\`.|\\..----...-'`   `-._.-'_.-'`");
+        printf("%s\n", "    /  ' `         ,       __.--'");
+        printf("%s\n", "    )/' _/     \\   `-_,   /        ");
+        printf("%s\n", "    `-'\" `\"\\_  ,_.-;_.-\\_ ',     ");
+        printf("%s\n", "        _.-'_./   {_.'   ; /");
+        printf("%s\n", "       {_.-``-'         {_/");
+    */
 }
